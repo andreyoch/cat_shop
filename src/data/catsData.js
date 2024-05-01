f33ffc4179bbd1e1e285c16a63864d59f70fea86
Andrzej Ciszewski <aw.ciszewski@protonmail.com>
2024-05-01 11:46:16 +0200

    refactor: change image location from google drive to localhost

--- src/data/catsData.js
+++ src/data/catsData.js
@@ -1,72 +1,84 @@
+import CatImage1 from "./images/1.jpg";
+import CatImage2 from "./images/2.jpg";
+import CatImage3 from "./images/3.jpg";
+import CatImage4 from "./images/4.jpeg";
+import CatImage5 from "./images/5.jpeg";
+import CatImage6 from "./images/6.jpeg";
+import CatImage7 from "./images/7.jpeg";
+import CatImage8 from "./images/8.png";
+import CatImage9 from "./images/9.jpeg";
+import CatImage10 from "./images/10.jpeg";
+
+
 export const catsData = [
     {
-        "id": 1,
-        "name": "Fluffy",
-        "price": 100,
-        "image": "https://drive.google.com/file/d/1DZzFdCEsJETHmVLpu41NMxWA8h4WrVXv/view?usp=drive_link",
-        "description": "A lovable ball of fur with a mischievous streak."
+        id: 1,
+        name: "Fluffy",
+        price: 100,
+        image: CatImage1,
+        description: "A lovable ball of fur with a mischievous streak.",
     },
     {
-        "id": 2,
-        "name": "Whiskers",
-        "price": 150,
-        "image": "https://drive.google.com/file/d/1bh9THYgosIVk8QjAisu5RE54nqaaj-iy/view?usp=drive_link",
-        "description": "A sophisticated feline with impeccable manners."
+        id: 2,
+        name: "Whiskers",
+        price: 150,
+        image: CatImage2,
+        description: "A sophisticated feline with impeccable manners.",
     },
     {
-        "id": 3,
-        "name": "Mittens",
-        "price": 200,
-        "image": "https://drive.google.com/file/d/1IIdxsYw8LQElzHnd14WWmmKf4ylt2QJo/view?usp=drive_link",
-        "description": "A cuddly companion who loves to play."
+        id: 3,
+        name: "Mittens",
+        price: 200,
+        image: CatImage3,
+        description: "A cuddly companion who loves to play.",
     },
     {
-        "id": 4,
-        "name": "Snowball",
-        "price": 120,
-        "image": "https://drive.google.com/file/d/1EjxNU259jR_p3jNfoSn04s-mq7tPoATV/view?usp=drive_link",
-        "description": "A graceful snow-white cat with a playful spirit."
+        id: 4,
+        name: "Snowball",
+        price: 120,
+        image: CatImage4,
+        description: "A graceful snow-white cat with a playful spirit.",
     },
     {
-        "id": 5,
-        "name": "Oreo",
-        "price": 180,
-        "image": "https://drive.google.com/file/d/1aQfXbbbIGKVvJF5hhH18vbvMHISBhcj_/view?usp=drive_link",
-        "description": "A fluffy and playful cat with a mischievous personality."
+        id: 5,
+        name: "Oreo",
+        price: 180,
+        image: CatImage5,
+        description: "A fluffy and playful cat with a mischievous personality.",
     },
     {
-        "id": 6,
-        "name": "Shadow",
-        "price": 90,
-        "image": "https://drive.google.com/file/d/1ir_X8Iz8yGQzua7ac-IaaSfHsIwLGTxT/view?usp=drive_link",
-        "description": "A mysterious and elusive feline companion."
+        id: 6,
+        name: "Shadow",
+        price: 90,
+        image: CatImage6,
+        description: "A mysterious and elusive feline companion.",
     },
     {
-        "id": 7,
-        "name": "Simba",
-        "price": 250,
-        "image": "https://drive.google.com/file/d/1AIHdSS8lujl3y9yV3Kte6bMxYA3bMH1r/view?usp=drive_link",
-        "description": "A majestic king of the jungle with a heart of gold."
+        id: 7,
+        name: "Simba",
+        price: 250,
+        image: CatImage7,
+        description: "A majestic king of the jungle with a heart of gold.",
     },
     {
-        "id": 8,
-        "name": "Luna",
-        "price": 160,
-        "image": "https://drive.google.com/file/d/11vbFPUbBvThJKObKqg-iOFrMY-L-kwYK/view?usp=drive_link",
-        "description": "A beautiful and mysterious cat who loves the night."
+        id: 8,
+        name: "Luna",
+        price: 160,
+        image: CatImage8,
+        description: "A beautiful and mysterious cat who loves the night.",
     },
     {
-        "id": 9,
-        "name": "Max",
-        "price": 140,
-        "image": "https://drive.google.com/file/d/1_XMMQ-NhXVJMwPkeZBX1553hbDFgKNtU/view?usp=drive_link",
-        "description": "A mischievous troublemaker with a heart of gold."
+        id: 9,
+        name: "Max",
+        price: 140,
+        image: CatImage9,
+        description: "A mischievous troublemaker with a heart of gold.",
     },
     {
-        "id": 10,
-        "name": "Bella",
-        "price": 220,
-        "image": "https://drive.google.com/file/d/1guRelvhaisE0qDIvDo9NjLQs1keQUpZX/view?usp=drive_link",
-        "description": "A graceful and elegant feline companion."
-    }
+        id: 10,
+        name: "Bella",
+        price: 220,
+        image: CatImage10,
+        description: "A graceful and elegant feline companion.",
+    },
 ];
