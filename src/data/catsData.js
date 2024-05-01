7b0f3121a20f3ec56738ede82c2d2d9be2feee6f
Andrzej Ciszewski <aw.ciszewski@protonmail.com>
2024-05-01 10:54:13 +0200

    refactor: add id property to cats data

--- src/data/catsData.js
+++ src/data/catsData.js
@@ -1,59 +1,69 @@
 export const catsData = [
     {
+        "id": 1,
         "name": "Fluffy",
         "price": 100,
         "image": "https://drive.google.com/file/d/1DZzFdCEsJETHmVLpu41NMxWA8h4WrVXv/view?usp=drive_link",
         "description": "A lovable ball of fur with a mischievous streak."
     },
     {
+        "id": 2,
         "name": "Whiskers",
         "price": 150,
         "image": "https://drive.google.com/file/d/1bh9THYgosIVk8QjAisu5RE54nqaaj-iy/view?usp=drive_link",
         "description": "A sophisticated feline with impeccable manners."
     },
     {
+        "id": 3,
         "name": "Mittens",
         "price": 200,
         "image": "https://drive.google.com/file/d/1IIdxsYw8LQElzHnd14WWmmKf4ylt2QJo/view?usp=drive_link",
         "description": "A cuddly companion who loves to play."
     },
     {
+        "id": 4,
         "name": "Snowball",
         "price": 120,
         "image": "https://drive.google.com/file/d/1EjxNU259jR_p3jNfoSn04s-mq7tPoATV/view?usp=drive_link",
         "description": "A graceful snow-white cat with a playful spirit."
     },
     {
+        "id": 5,
         "name": "Oreo",
         "price": 180,
         "image": "https://drive.google.com/file/d/1aQfXbbbIGKVvJF5hhH18vbvMHISBhcj_/view?usp=drive_link",
         "description": "A fluffy and playful cat with a mischievous personality."
     },
     {
+        "id": 6,
         "name": "Shadow",
         "price": 90,
         "image": "https://drive.google.com/file/d/1ir_X8Iz8yGQzua7ac-IaaSfHsIwLGTxT/view?usp=drive_link",
         "description": "A mysterious and elusive feline companion."
     },
     {
+        "id": 7,
         "name": "Simba",
         "price": 250,
         "image": "https://drive.google.com/file/d/1AIHdSS8lujl3y9yV3Kte6bMxYA3bMH1r/view?usp=drive_link",
         "description": "A majestic king of the jungle with a heart of gold."
     },
     {
+        "id": 8,
         "name": "Luna",
         "price": 160,
         "image": "https://drive.google.com/file/d/11vbFPUbBvThJKObKqg-iOFrMY-L-kwYK/view?usp=drive_link",
         "description": "A beautiful and mysterious cat who loves the night."
     },
     {
+        "id": 9,
         "name": "Max",
         "price": 140,
         "image": "https://drive.google.com/file/d/1_XMMQ-NhXVJMwPkeZBX1553hbDFgKNtU/view?usp=drive_link",
         "description": "A mischievous troublemaker with a heart of gold."
     },
     {
+        "id": 10,
         "name": "Bella",
         "price": 220,
         "image": "https://drive.google.com/file/d/1guRelvhaisE0qDIvDo9NjLQs1keQUpZX/view?usp=drive_link",
