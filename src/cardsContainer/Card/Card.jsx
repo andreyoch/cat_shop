981a0f666726b845850f6dd8d9c00e28d8148739
Andrzej Ciszewski <aw.ciszewski@protonmail.com>
2024-05-01 12:22:07 +0200

    feature: add redirection after button click

--- src/cardsContainer/Card/Card.jsx
+++ src/cardsContainer/Card/Card.jsx
@@ -1,6 +1,10 @@
-
 /*eslint-disable*/ // need to disable because of eslint error proptype
-const Card = ({ name, description, image, price}) => {
+
+const Card = ({ name, description, image, price }) => {
+  const handleClick = () => {
+    window.location.href =
+      "https://www.avito.ru/moskva?q=%D0%BA%D0%BE%D1%82%D1%8B";
+  };
   return (
     <div className="card card-compact m-3 w-96 bg-base-100 shadow-xl">
       <figure className="h-60">
@@ -10,7 +14,9 @@ const Card = ({ name, description, image, price}) => {
         <h2 className="card-title">{name}</h2>
         <p>{description}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now {price}$</button>
+          <button className="btn btn-primary" onClick={handleClick}>
+            Buy Now {price}$
+          </button>
         </div>
       </div>
     </div>
