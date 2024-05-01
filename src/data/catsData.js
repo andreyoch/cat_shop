30ff8a389590d50a4c053ae857c05fe004c05ab1
Andrzej Ciszewski <aw.ciszewski@protonmail.com>
2024-05-01 09:59:13 +0200

    feature: add data about cats

---
+++ src/data/catsData.js
@@ -0,0 +1,52 @@
+export const catsData = [
+    {
+        "name": "Fluffy",
+        "price": 100,
+        "image": "https://drive.google.com/file/d/1DZzFdCEsJETHmVLpu41NMxWA8h4WrVXv/view?usp=drive_link"
+    },
+    {
+        "name": "Whiskers",
+        "price": 150,
+        "image": "https://drive.google.com/file/d/1bh9THYgosIVk8QjAisu5RE54nqaaj-iy/view?usp=drive_link"
+    },
+    {
+        "name": "Mittens",
+        "price": 200,
+        "image": "https://drive.google.com/file/d/1IIdxsYw8LQElzHnd14WWmmKf4ylt2QJo/view?usp=drive_link"
+    },
+    {
+        "name": "Snowball",
+        "price": 120,
+        "image": "https://drive.google.com/file/d/1EjxNU259jR_p3jNfoSn04s-mq7tPoATV/view?usp=drive_link"
+    },
+    {
+        "name": "Oreo",
+        "price": 180,
+        "image": "https://drive.google.com/file/d/1aQfXbbbIGKVvJF5hhH18vbvMHISBhcj_/view?usp=drive_link"
+    },
+    {
+        "name": "Shadow",
+        "price": 90,
+        "image": "https://drive.google.com/file/d/1ir_X8Iz8yGQzua7ac-IaaSfHsIwLGTxT/view?usp=drive_link"
+    },
+    {
+        "name": "Simba",
+        "price": 250,
+        "image": "https://drive.google.com/file/d/1AIHdSS8lujl3y9yV3Kte6bMxYA3bMH1r/view?usp=drive_link"
+    },
+    {
+        "name": "Luna",
+        "price": 160,
+        "image": "https://drive.google.com/file/d/11vbFPUbBvThJKObKqg-iOFrMY-L-kwYK/view?usp=drive_link"
+    },
+    {
+        "name": "Max",
+        "price": 140,
+        "image": "https://drive.google.com/file/d/1_XMMQ-NhXVJMwPkeZBX1553hbDFgKNtU/view?usp=drive_link"
+    },
+    {
+        "name": "Bella",
+        "price": 220,
+        "image": "https://drive.google.com/file/d/1guRelvhaisE0qDIvDo9NjLQs1keQUpZX/view?usp=drive_link"
+    }
+]
